2fc451f06193067c5b08dbe51787c2cb396706e3
Duncan Lew <duncan@lew.email>
2019-08-29 21:57:36 +0200

    created app.route

--- project.py
+++ project.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def entry_point():
+    return "HELLO"
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run(port=8080)
