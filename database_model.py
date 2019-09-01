8cc4aeaf3139512132f349cdf0030b8bf6693b56
Duncan Lew <duncan@lew.email>
2019-09-01 16:57:08 +0200

    user relationship added in ComputerShop (+1 squashed commit) Squashed commits: [5675d23] user model added

--- database_model.py
+++ database_model.py
@@ -1,14 +1,30 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, create_engine
 from sqlalchemy.orm import relationship
+from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.hash(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
 
 class ComputerShop(Base):
     __tablename__ = "computer_shop"
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
 class Product(Base):
@@ -17,5 +33,9 @@ class Product(Base):
     name = Column(String(80), nullable=False)
     description = Column(String(250))
     price = Column(String(8))
-    computer_shop = relationship(ComputerShop)
     computer_shop_id = Column(Integer, ForeignKey('computer_shop.id'))
+    computer_shop = relationship(ComputerShop)
+
+
+engine = create_engine('sqlite:///computer_shop.db')
+Base.metadata.create_all(engine)
