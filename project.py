b042760cf44f66f94ead74a66d720237408d067b
Duncan Lew <duncan@lew.email>
2019-09-27 17:14:09 +0200

    extra todo added

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from database_model import Base, User, ComputerShop, Product
 
 app = Flask(__name__)
-app.secret_key = "super secret key"
+app.secret_key = "super secret key"  # TODO what to do with the secret key
 
 engine = create_engine('sqlite:///computer_shop.db')
 Base.metadata.bind = engine
