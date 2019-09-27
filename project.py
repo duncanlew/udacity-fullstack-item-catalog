b0d00974425ece84d387d3d28953ab14424ea602
Duncan Lew <duncan@lew.email>
2019-09-27 21:53:06 +0200

    basic things for oauth

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session as flask_session
+from flask_oauth import OAuth
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -6,12 +7,31 @@ from database_model import Base, User, ComputerShop, Product
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
+oauth = OAuth()
 
 engine = create_engine('sqlite:///computer_shop.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 db_session = DBSession()
 
+# Use Twitter as example remote application
+twitter = oauth.remote_app('twitter',
+                           # unless absolute urls are used to make requests, this will be added
+                           # before all URLs.  This is also true for request_token_url and others.
+                           base_url='https://api.twitter.com/1/',
+                           # where flask should look for new request tokens
+                           request_token_url='https://api.twitter.com/oauth/request_token',
+                           # where flask should exchange the token with the remote application
+                           access_token_url='https://api.twitter.com/oauth/access_token',
+                           # twitter knows two authorizatiom URLs.  /authorize and /authenticate.
+                           # they mostly work the same, but for sign on /authenticate is
+                           # expected because this will give the user a slightly different
+                           # user interface on the twitter side.
+                           authorize_url='https://api.twitter.com/oauth/authenticate',
+                           # the consumer keys from the twitter application registry.
+                           consumer_key='ADD TWITTER CONSUMER KEY',
+                           consumer_secret='ADD TWITTER CONSUMER SECRET'
+                           )
 
 # TODO rename entry_point to home
 @app.route("/")
