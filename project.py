40ceecd4833bc29a3a623d4fb3ca5c6318968dfe
Duncan Lew <duncan@lew.email>
2019-09-29 22:10:28 +0200

    more refinements

--- project.py
+++ project.py
@@ -16,21 +16,12 @@ db_session = DBSession()
 
 # Use Twitter as example remote application
 twitter = oauth.remote_app('twitter',
-                           # unless absolute urls are used to make requests, this will be added
-                           # before all URLs.  This is also true for request_token_url and others.
                            base_url='https://api.twitter.com/1/',
-                           # where flask should look for new request tokens
                            request_token_url='https://api.twitter.com/oauth/request_token',
-                           # where flask should exchange the token with the remote application
                            access_token_url='https://api.twitter.com/oauth/access_token',
-                           # twitter knows two authorizatiom URLs.  /authorize and /authenticate.
-                           # they mostly work the same, but for sign on /authenticate is
-                           # expected because this will give the user a slightly different
-                           # user interface on the twitter side.
                            authorize_url='https://api.twitter.com/oauth/authenticate',
-                           # the consumer keys from the twitter application registry.
-                           consumer_key='ADD TWITTER CONSUMER KEY',
-                           consumer_secret='ADD TWITTER CONSUMER SECRET'
+                           consumer_key='YOUR_CONSUMER_KEY',
+                           consumer_secret='YOUR_CONSUMER_SECRET'
                            )
 
 
@@ -187,6 +178,7 @@ def oauth_authorized(resp):
     myRequest = request
     myResponse = resp
     if resp is None:
+        # TODO remove this or add something in html
         # flash(u'You denied the request to sign in.')
         return redirect(next_url)
 
